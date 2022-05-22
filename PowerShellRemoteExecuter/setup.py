2ee47199ed7b017ce2f3634d76a0f0c44fab669f
danielm <mp3istk@gmail.com>
2022-05-22 15:50:20 +0300

    connector path fixed

--- PowerShellRemoteExecuter/setup.py
+++ PowerShellRemoteExecuter/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DESCRIPTION= "Execute PowerShell script on remote host"
 LONG_DESCRIPTION = "Github page: https://github.com/danielMandelblat/PythonPackages/tree/master/PowerShellRemoteExecuter "
 
